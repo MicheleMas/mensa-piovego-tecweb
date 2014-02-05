d266e70e5a7b269ad72161244c4dbb126a122505
gian.boscolo@gmail.com <gian.boscolo@gmail.com@c7c2ffdb-7700-2f77-9eed-6512ff1a737e>
2014-02-05 21:19:04 +0000

    modificato un po' il footer. Ho aggiunto anche un piccolo javascript sull'evento hover del logo di facebook, possiamo prenderlo come spunto per visualizzare delle anteprime su certi oggetti (ho creato la cartella scripts)

---
+++ trunk/scripts/index.js
@@ -0,0 +1,5 @@
+function showDiv(){
+	document.getElementById("facebook_info").style.display='block';
+	window.setTimeout(function(){
+		document.getElementById("facebook_info").style.display = 'none';},2000);
+}
