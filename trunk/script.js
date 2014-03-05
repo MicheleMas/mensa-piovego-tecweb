30e69a2bb791c032cad26cc6b24d4889309f06f1
mick89.mac@gmail.com <mick89.mac@gmail.com@c7c2ffdb-7700-2f77-9eed-6512ff1a737e>
2014-03-05 14:17:16 +0000

    Aggiunta la sostituzione della mappa via JS in where.html

--- trunk/script.js
+++ trunk/script.js
@@ -12,3 +12,8 @@ function addDayColor() {
 		element.setAttribute("class", 'current_day');
 	};
 }
+
+function replaceMap() {
+	var map = document.getElementById('mapVisualization');
+	map.innerHTML = "<iframe id='Gmap_frame' src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1400.460291941266!2d11.889645!3d45.410941!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2c00159331ead3d8!2sRistorEsu+Nord+Piovego!5e0!3m2!1sit!2sit!4v1394028372446'></iframe>";
+}
