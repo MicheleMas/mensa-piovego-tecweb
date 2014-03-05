5b242c5bcb2b88d99b2de7e99050c9462a5eec00
mick89.mac@gmail.com <mick89.mac@gmail.com@c7c2ffdb-7700-2f77-9eed-6512ff1a737e>
2014-03-05 14:50:22 +0000

    Aggiunti gli script di apertura su tab alle pagine in italiano

--- trunk/script.js
+++ trunk/script.js
@@ -1,3 +1,20 @@
+function openTab() {
+	var links = document.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++) {
+		if(links[i].getAttribute("class") == "popup") {
+			links[i].onclick = function() {
+				popUp(this.getAttribute("href"));
+				return false;
+			}
+		}
+	}
+}
+
+function popUp(url)
+{
+	window.open(url, "nuovaFinestra");
+}
+
 function addDayColor() {
 	var date = new Date();
 	var day = date.getDay();
